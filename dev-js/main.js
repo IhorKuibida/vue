d54ee3b1f3fbd33b6d63bc4383fd522f2d2e0bfe
IhorKuibida <megasho@ukr.net>
2020-01-16 00:36:49 +0200

    new lesson from vue courses vue

--- dev-js/main.js
+++ dev-js/main.js
@@ -3,12 +3,21 @@ var app = new Vue({
     data:{
         user: "Ihor",
         link:"https://google.com",
+        open: 10,
+        colors:['green', 'red', 'blue']
+    },
+    computed:{
+        title : function() {
+            return this.user +" "+this.open;
+        }
     },
     methods:{
-        changeTitle: function (e){ this.user = das()}
+        changeTitle: function (e){ this.open = e.target.value}
+        
     }
 });
 
 let das = () => {
-    return "hello world"
+    return "hello world";
+   
 }
